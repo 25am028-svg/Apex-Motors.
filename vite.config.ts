f882797d0d18b324a526d76d7b771d521ec12080
25am028-svg <25am028@skcet.ac.in>
2026-03-15 20:48:47 +0530

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Apex-Motors/', // <--- ADD THIS LINE EXACTLY
+    base: '/Apex-Motors./', // <--- ADD THIS LINE EXACTLY
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
